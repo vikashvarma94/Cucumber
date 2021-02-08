f31c5c3a8c78daa6f300c8071500a5dcdeaf40ef
151508 <151508@A073DHYD.ustr.com>
2021-02-08 20:37:46 +0530

    08/02/2021

--- Cucumber/src/test/java/MyRunner/Testrunner.java
+++ Cucumber/src/test/java/MyRunner/Testrunner.java
@@ -4,15 +4,16 @@ import org.junit.runner.RunWith;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
-
-	@RunWith(Cucumber.class)
-	@CucumberOptions(
-			features = "C:\\Users\\151508\\git\\Cucumber\\Cucumber\\src\\test\\resources\\Features\\login.feature", //the path of the feature files
-			glue={"Stepdefinitions"},
-			//plugin= {"pretty","html:test-outout", "json:json_output/cucumber.json", "junit:junit_xml/cucumber.xml"}, //to generate different types of reporting
-			monochrome = true, //display the console output in a proper readable format
-			dryRun = false
-			//tags = {"@RegressionTest"}
-			)
-	public class Testrunner {
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		features = "C:\\Users\\151508\\git\\Cucumber\\Cucumber\\src\\test\\resources\\Features", // the path of feature		\\login.feature																																																	// feature																													// files
+		glue = { "Stepdefinitions" },
+		plugin = { "pretty",
+				"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+				"timeline:test-output-thread/" }, // to generate different types of reporting
+		monochrome = true, // display the console output in a proper readable format
+		dryRun = false
+		// tags = {"@RegressionTest"}
+)
+public class Testrunner {
 }
